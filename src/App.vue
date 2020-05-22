ac95c7cebf8816f441b7d22d963282e57ec9957b
romaincapelle <romain.capelle@gmail.com>
2020-05-22 15:40:48 -0700

    deleted HelloWorld Component

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,13 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
-    <helloworld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
-import helloworld from '../src/components/helloworld';
-
 export default {
   name: 'App',
-  components: {
-    helloworld,
-  },
+  components: {},
 };
 </script>
 
